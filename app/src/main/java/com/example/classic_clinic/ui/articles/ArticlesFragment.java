1c7de58d94bf9f16115445c2653d08ae3c30d33f
Mind-Creator <sir.brave.zoidberg@gmail.com>
2021-08-03 19:56:46 +0700

    updated

---
+++ app/src/main/java/com/example/classic_clinic/ui/articles/ArticlesFragment.java
@@ -0,0 +1,38 @@
+package com.example.classic_clinic.ui.articles;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.classic_clinic.R;
+
+public class ArticlesFragment extends Fragment {
+
+    private ArticlesViewModel mViewModel;
+
+    public static ArticlesFragment newInstance() {
+        return new ArticlesFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.articles_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ArticlesViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
